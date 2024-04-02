53e9b21ec4f7ee045a6c93d2a1aeaf8235224ec8
Daniel-Oliveira0 <dnvinicius00@gmail.com>
2024-04-01 23:34:48 -0300

    first commit

---
+++ src/js/script.js
@@ -0,0 +1,41 @@
+const formulario = document.querySelector(".formulario");
+const inputs = document.querySelectorAll(".input, .textarea");
+const campoObrigatório = document.querySelectorAll(".campo-obrigatorio");
+
+formulario.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  inputs.forEach((input) => {
+    const paragrafo = input.nextElementSibling;
+    if (input.value === "") {
+      input.style.border = "2px solid red";
+      paragrafo.style.color = "red"
+      paragrafo.classList.remove("esconder");
+    } else {
+      input.style.border = "2px solid green";
+      paragrafo.classList.add("esconder");
+    }
+  });
+});
+
+inputs.forEach((input) => {
+  input.addEventListener("input", () => {
+    const paragrafo = input.nextElementSibling;
+    if (input.value === "") {
+      input.style.border = "2px solid red";
+      paragrafo.style.color = "red"
+      paragrafo.classList.remove("esconder");
+    } else {
+      input.style.border = "2px solid green";
+      paragrafo.classList.add("esconder");
+    }
+  });
+});
+
+function verificarInput(input) {
+  if (input.value === "") {
+    input.style.border = "2px solid red";
+  } else {
+    input.style.border = "2px solid green";
+  }
+}
